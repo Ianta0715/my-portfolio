7cb6699c8ae42a86e6dceb3ec35d92680c7f9340
Ian Tarquini <ianricar4@gmail.com>
2024-10-17 14:25:48 -0300

    agregado los enlaces

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,6 +5,7 @@ import { ProyectsComponent } from '@/components/Proyects';
 import { Tecnologies } from '@/components/Tecnologies';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Link from 'next/link';
 
 
  const HomePage = () => {
@@ -17,14 +18,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
     <h3 className="font-semibold mt-2 text-blue-500">Full Stack/Frontend Developer</h3>
     <p className="mt-2 text-lg">I am a junior frontend developer . My passion is to create attractive and functional web interfaces that provide amazing experiences for users. I enjoy combining modern design with clean code to ensure that each project not only looks good but also functions efficiently. I am always looking for new challenges and opportunities to continue learning and growing in the world of frontend development..</p>
     <div className="flex flex-row lg:justify-start space-x-4 mt-8">
+      <Link href='https://www.linkedin.com/in/ian-tarquini-b83b72247/' target='_blank' rel='noopener noreferrer'>
   <button className=" text-white px-4 py-2 rounded transition-transform duration-700 ease-in-out transform hover:scale-95 hover:bg-blue-600 flex items-center"> 
     <FontAwesomeIcon icon={faLinkedin} className="mr-2 w-6 h-6" /> 
     LinkedIn
   </button>
+      </Link>
+      <Link href='https://github.com/Ianta0715' target='_blank' rel='noopener noreferrer' >
   <button style={{backgroundColor: '#7A42C6'}} className=" text-white px-4 py-2 rounded transition-transform duration-700 ease-in-out transform hover:scale-95 hover:bg-gray-700 flex items-center">
     <FontAwesomeIcon icon={faGithub} className="mr-2 w-6 h-6" /> 
     GitHub
   </button>
+      </Link>
 </div>
   </div>
 
