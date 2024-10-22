409bba5477acb35755fb346718cc0621eb6cb497
Ian Tarquini <ianricar4@gmail.com>
2024-10-21 23:17:33 -0300

    terminadoooo

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -16,7 +16,7 @@ import Link from 'next/link';
   <div className="lg:w-1/2 text-center lg:text-left">
     <h1 className="text-3xl font-bold">Hi, I&apos;m Ian Tarquini</h1>
     <h3 className="font-semibold mt-2 text-blue-500">Full Stack/Frontend Developer</h3>
-    <p className="mt-2 text-lg">I am a junior frontend developer . My passion is to create attractive and functional web interfaces that provide amazing experiences for users. I enjoy combining modern design with clean code to ensure that each project not only looks good but also functions efficiently. I am always looking for new challenges and opportunities to continue learning and growing in the world of frontend development..</p>
+    <p className="mt-2 text-lg">I am a frontend developer . My passion is to create attractive and functional web interfaces that provide amazing experiences for users. I enjoy combining modern design with clean code to ensure that each project not only looks good but also functions efficiently. I am always looking for new challenges and opportunities to continue learning and growing in the world of frontend development..</p>
     <div className="flex flex-row lg:justify-start space-x-4 mt-8">
       <Link href='https://www.linkedin.com/in/ian-tarquini-b83b72247/' target='_blank' rel='noopener noreferrer'>
   <button className=" text-white px-4 py-2 rounded transition-transform duration-700 ease-in-out transform hover:scale-95 hover:bg-blue-600 flex items-center"> 
