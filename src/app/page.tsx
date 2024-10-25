e54933353c19a87122a413df7f9d6a2f997f9a30
Ian Tarquini <ianricar4@gmail.com>
2024-10-25 12:06:08 -0300

    terminado con cambios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
-import HomePage from "./home/page";
+import Link from "next/link";
 
 
 const LandingPage:React.FC = () => {
 
   return (
     <>
-    <HomePage/>
+    <Link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet"/>
+
+<div className="content">
+  <div className="content__container">
+    <p className="content__container__text">
+      Hello
+    </p>
+    
+    <ul className="content__container__list">
+      <li className="content__container__list__item">world !</li>
+      <li className="content__container__list__item">bob !</li>
+      <li className="content__container__list__item">users !</li>
+      <li className="content__container__list__item">everybody !</li>
+    </ul>
+  </div>
+</div>
+<div>
+  <Link href='/home'>
+  <button className="redirect-button">Go to page</button>
+  </Link>
+</div>
     </>
   )
 }
