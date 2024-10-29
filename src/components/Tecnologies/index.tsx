9d787d61fa0fbe9304e41ac5b4205f9b3790dae0
Ian Tarquini <ianricar4@gmail.com>
2024-10-29 12:10:27 -0300

    agregado mas estilos

--- src/components/Tecnologies/index.tsx
+++ src/components/Tecnologies/index.tsx
@@ -1,76 +1,53 @@
-
+"use client";
 import Image from 'next/image';
 import Link from 'next/link';
-
+import { motion } from 'framer-motion';
 
 export const Tecnologies = () => {
+  const techVariants = {
+    hidden: { opacity: 0, x: -50 },
+    visible: { opacity: 1, x: 0 },
+  };
+
   return (
     <div>
-       
-  <h2 className="text-center text-5xl mb-6 p-5">Technologies</h2>
-  <div className="technologies-container">
-  <div className="technology-category my-4">
-
-    <div className="tech-icons grid grid-cols-4 gap-10 justify-center">
-      <Link href="https://nextjs.org/" target="_blank" rel="noopener noreferrer" >
-        <Image src="/assets/nextJS.png" alt="NextJS" width={192} height={192} className="rounded-full w-32 h-32 object-cover" />
-      </Link>
-      <Link href="https://reactjs.org/" target="_blank" rel="noopener noreferrer" >
-        <Image src="/assets/react.png" alt="React" width={192} height={192} className="rounded-full w-32 h-32 object-contain" />
-      </Link>
-      <Link href="https://www.w3schools.com/css/" target="_blank" rel="noopener noreferrer" >
-        <Image src="/assets/css3.png" alt="CSS3" width={192} height={192} className="rounded-full w-32 h-32 object-cover" />
-      </Link>
-      <Link href="https://en.wikipedia.org/wiki/HTML5" target="_blank" rel="noopener noreferrer" >
-        <Image src="/assets/html5.png" alt="HTML5" width={192} height={192} className="rounded-full w-32 h-32 object-contain" />
-      </Link>
-      <Link href="https://www.javascript.com/" target="_blank" rel="noopener noreferrer">
-        <Image src="/assets/js.png" alt="JavaScript" width={192} height={192} className="rounded-full w-32 h-32 object-cover" />
-      </Link>
-      <Link href="https://www.typescriptlang.org/" target="_blank" rel="noopener noreferrer" >
-        <Image src="/assets/ts.png" alt="TypeScript" width={192} height={192} className="rounded-full w-32 h-32 object-cover" />
-      </Link>
-      <Link href="https://redux.js.org/" target="_blank" rel="noopener noreferrer" >
-        <Image src="/assets/redux.png" alt="Redux" width={192} height={192} className="rounded-full w-32 h-32 object-cover" />
-      </Link>
-      <Link href="https://www.tailwindcss.com/" target="_blank" rel="noopener noreferrer" >
-        <Image src="/assets/tailwind.jpg" alt="Tailwind CSS" width={192} height={192} className="rounded-full w-32 h-32 object-cover" />
-      </Link>
-      <Link href="https://mui.com/" target="_blank" rel="noopener noreferrer" >
-        <Image src="/assets/mui.png" alt="Material UI" width={192} height={192} className="rounded-full w-32 h-32 object-cover" />
-      </Link>
-      <Link href="https://www.figma.com/" target="_blank" rel="noopener noreferrer" >
-        <Image src="/assets/figma.png" alt="Figma" width={192} height={192} className="rounded-full w-32 h-32 object-contain" />
-      </Link>
-      <Link href="https://nodejs.org/" target="_blank" rel="noopener noreferrer" >
-        <Image src="/assets/node.png" alt="Node.js" width={192} height={192} className="rounded-full w-32 h-32 object-contain" />
-      </Link>
-      <Link href="https://expressjs.com/" target="_blank" rel="noopener noreferrer" >
-        <Image src="/assets/express.png" alt="Express.js" width={192} height={192} className="rounded-full w-32 h-32 object-cover" />
-      </Link>
-      <Link href="https://www.mongodb.com/" target="_blank" rel="noopener noreferrer" >
-        <Image src="/assets/mongodb1.png" alt="mongoDB" width={192} height={192} className="rounded-full w-32 h-32 object-cover" />
-      </Link>
-      <Link href="https://www.postgresql.org/" target="_blank" rel="noopener noreferrer" >
-        <Image src="/assets/postgreSQL2.png" alt="PostgreSQL" width={192} height={192} className="rounded-full w-32 h-32 object-contain" />
-      </Link> 
-      <Link href="https://github.com/" target="_blank" rel="noopener noreferrer" >
-        <Image src="/assets/git.png" alt="Git" width={192} height={192} className="rounded-full w-32 h-32 object-cover" />
-      </Link>
-      <Link href="https://www.gnu.org/software/bash/" target="_blank" rel="noopener noreferrer" >
-        <Image src="/assets/gnubash.png" alt="Bash" width={192} height={192} className="rounded-full w-32 h-32 object-cover" />
-      </Link>
+      <h2 className="text-center text-5xl mb-6 p-5">Technologies</h2>
+      <div className="technologies-container">
+        <div className="technology-category my-4">
+          <div className="tech-icons grid grid-cols-4 gap-10 justify-center">
+            {[
+              { src: "/assets/nextJS.png", alt: "NextJS", link: "https://nextjs.org/" },
+              { src: "/assets/react.png", alt: "React", link: "https://reactjs.org/" },
+              { src: "/assets/css3.png", alt: "CSS3", link: "https://www.w3schools.com/css/" },
+              { src: "/assets/html5.png", alt: "HTML5", link: "https://en.wikipedia.org/wiki/HTML5" },
+              { src: "/assets/js.png", alt: "JavaScript", link: "https://www.javascript.com/" },
+              { src: "/assets/ts.png", alt: "TypeScript", link: "https://www.typescriptlang.org/" },
+              { src: "/assets/redux.png", alt: "Redux", link: "https://redux.js.org/" },
+              { src: "/assets/tailwind.jpg", alt: "Tailwind CSS", link: "https://www.tailwindcss.com/" },
+              { src: "/assets/mui.png", alt: "Material UI", link: "https://mui.com/" },
+              { src: "/assets/figma.png", alt: "Figma", link: "https://www.figma.com/" },
+              { src: "/assets/node.png", alt: "Node.js", link: "https://nodejs.org/" },
+              { src: "/assets/express.png", alt: "Express.js", link: "https://expressjs.com/" },
+              { src: "/assets/mongodb1.png", alt: "mongoDB", link: "https://www.mongodb.com/" },
+              { src: "/assets/postgreSQL2.png", alt: "PostgreSQL", link: "https://www.postgresql.org/" },
+              { src: "/assets/git.png", alt: "Git", link: "https://github.com/" },
+              { src: "/assets/gnubash.png", alt: "Bash", link: "https://www.gnu.org/software/bash/" },
+            ].map((tech, index) => (
+              <Link key={tech.alt} href={tech.link} target="_blank" rel="noopener noreferrer">
+                <motion.div
+                  initial="hidden"
+                  whileInView="visible"
+                  variants={techVariants}
+                  transition={{ duration: 0.5, delay: index * 0.1 }}
+                  
+                >
+                  <Image src={tech.src} alt={tech.alt} width={192} height={192} className="rounded-full w-32 h-32 object-cover" />
+                </motion.div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-
-  
-   
-    
-    
-  
-</div>
-
-
-    </div>
-  )
-}
+  );
+};
